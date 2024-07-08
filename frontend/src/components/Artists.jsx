508b716d806905a0ae310fdacf8c6165a35c5755
Giulia Hossu <ioana.hossu@yahoo.com>
2024-07-08 12:51:22 +0300

    Show album information and tracklist.

--- frontend/src/components/Artists.jsx
+++ frontend/src/components/Artists.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const Artists = () => {
+function Artists() {
   const [artists, setArtists] = useState([]);
+  const [expandedAlbum, setExpandedAlbum] = useState(null);
 
   useEffect(() => {
     const fetchArtists = async () => {
@@ -18,21 +19,45 @@ const Artists = () => {
     fetchArtists();
   }, []);
 
-  return (
+  const handleToggleAlbum = (artistName, albumTitle) => {
+    const albumId = `${artistName}-${albumTitle}`;
+    setExpandedAlbum(expandedAlbum === albumId ? null : albumId);
+  };
 
-    <section id="artists" className="row row-cols-sm-12 row-cols-md-2 row-cols-lg-4 g-3">
+  return (
+    <section id="artists" className="row">
       {artists.map(artist => (
-          <article key={artist._id} className="col card">
-            <h3 className="card-title">{artist.name}</h3>
-            <ul className=" list-group-flush">
-              {artist.albums.map(album => (
-                <li key={album._id} className="list-group-item">{album.title}<button className="btn"><i className="fa-solid fa-play"></i> </button></li>
-              ))}
-            </ul>
-          </article>
+        <article key={artist.name} className="card col-lg-3 col-md-6 col-sm-12 col-xs-12">
+          <h3 className="card-title">{artist.name}</h3>
+          <ul className="list-group list-group-flush">
+            {artist.albums.map(album => (
+              <li key={album.title} className="list-group-item">
+                <h6 className={expandedAlbum === `${artist.name}-${album.title}` ? 'fw-bold' : ''}>
+                  {album.title}
+                </h6>
+                <button
+                  className={`btn ms-2 ${expandedAlbum === `${artist.name}-${album.title}` ? 'rotate-icon' : ''}`}
+                  onClick={() => handleToggleAlbum(artist.name, album.title)}
+                >
+                  <i className="fa-solid fa-play"></i>
+                </button>
+                {expandedAlbum === `${artist.name}-${album.title}` && (
+                  <section>
+                    <p><em>{album.description}</em></p>
+                    <p><strong>Songs:</strong></p>
+                    <ol>
+                      {album.songs.map(song => (
+                        <li key={song._id}>{song.title} <span>{song.length}</span></li>
+                      ))}
+                    </ol>
+                  </section>
+                )}
+              </li>
+            ))}
+          </ul>
+        </article>
       ))}
     </section>
-
   );
 };
 
